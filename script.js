c658a6e0b85214301cae31341be8830a9aa0ab11
Devansh Sood <sooddevansh143.gmail.com>
2025-12-04 23:44:01 +0530

    section 1 questions

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
 
 //q1
 
-let user={
-    name:'dev',
-    email: 'A@amail.com',
 
-  login:function(){
-    console.log("logged in");
-  },
-}
+
+// let user={
+//     name:'dev',
+//     email: 'A@amail.com',
+
+//   login:function(){
+//     console.log("logged in");
+//   },
+// }
+
+// user.login();
+
+
+
+
+
+// now making it with class for 5 users
+
+
+// class User{
+
+//     constructor(name,email){
+
+// this.name=name;
+// this.email=email;
+    
+//     }
+
+
+//     login(){
+//         console.log("logged in");
+//     }
+// }
+
+
+// let u1=new User("dev","ad@.com");
+// let u2=new User("devansh","ad@.com")
+// let u3=new User("ved","ad@.com")
+// let u4=new User("vedansh","ad@.com")
+// let u5=new User("aarav","ad@.com")
+
+
+
+
+
+// q2
+
+
+// let product={
+
+//     name:'shoes',
+//     price:2000,
+//     discountedPrice:function(){
+
+//      return this.price-200;
+//     }
+
+// };
+
+// console.log(product.discountedPrice());
